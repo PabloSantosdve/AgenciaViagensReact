32548bab4fd982d4c2d0e29a487c553dcf70ab3b
Ka1quee <kaiqueoliveira375@gmail.com>
2024-04-24 18:55:56 -0300

    Primeiro commit

---
+++ src/assets/Components/Escocia.jsx
@@ -0,0 +1,11 @@
+import styles from './escocia.module.css'
+
+function Escocia(){
+    return(
+        <div className={styles.container}>
+            <h1>Venha conhecer a cultura da Escócia</h1>
+        </div>
+    )
+}
+
+export default Escocia
