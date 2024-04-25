3a127b5c3e7509fe46a5ba7ebc3a061103cbd3c0
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-04-25 12:53:38 -0300

    .

--- src/assets/Components/ComponenteProps.jsx
+++ src/assets/Components/ComponenteProps.jsx
@@ -1,17 +1,17 @@
 import Styles from './ComponenteProps.module.css'
 
-function ComponenteProps(lugar, texto, bandeira){
-    return(
-      <div className={Styles.corpo_texto}>
-          
-                <h2>Venha Explorar esse lugar incrivel, venha conhecer {lugar}</h2>
-          
-                <p>{texto}</p>
-          
-                <img className={Styles.imagem} src={bandeira} alt="bandeira" />
-          
-      </div>
-      
-    )
+function ComponenteProps({lugar, texto, bandeira}) {
+  return (
+    <div className={Styles.corpo_texto}>
+
+      <h2>Venha Explorar esse lugar incrivel, venha conhecer {lugar}</h2>
+
+      <p>{texto}</p>
+
+      <img className={Styles.imagem} src={bandeira} alt="bandeira" />
+
+    </div>
+
+  )
 }
 export default ComponenteProps
--- src/assets/Components/Escocia.jsx
+++ src/assets/Components/Escocia.jsx
@@ -1,10 +1,12 @@
 import Styles from './escocia.module.css'
-import ComponenteProps from './ComponenteProps'
+import ComponenteProps from './ComponenteProps' 
 
 function Escocia(){
     return(
         <div className={Styles.container}>
-         <ComponenteProps />
+         <ComponenteProps 
+         lugar="Escocia"
+         texto="A Escócia é famosa por seus castelos impressionantes, com mais de 2.000 espalhados por todo o país. Entre os mais conhecidos estão o Castelo de Edimburgo, o Castelo de Stirling e o Castelo de Urquhart, à beira do Lago Ness."/>
         </div>
     )
 }
